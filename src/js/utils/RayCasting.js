72b12ed2cf3f082e8993443f47f71cdefab969ef
scottrw93 <scottrw93@gmail.com>
2021-01-05 20:38:27 +0000

    Fix

--- src/js/utils/RayCasting.js
+++ src/js/utils/RayCasting.js
@@ -1,15 +1,15 @@
 export const inside = (point, vs) => {
-  var x = point.x,
+  const x = point.x,
     y = point.y;
 
-  var inside = false;
-  for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
-    var xi = vs[i].x,
+  let inside = false;
+  for (let i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+    const xi = vs[i].x,
       yi = vs[i].y;
-    var xj = vs[j].x,
+    const xj = vs[j].x,
       yj = vs[j].y;
 
-    var intersect = yi > y != yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
+    const intersect = yi > y !== yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
     if (intersect) inside = !inside;
   }
 
