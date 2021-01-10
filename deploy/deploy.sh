de00b5ec8d424c4d416c81f9e111a1193dca6fda
scottrw93 <scottrw93@gmail.com>
2021-01-10 13:34:37 +0000

    Add key

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd build
-git clone -i ../private git@github.com:scottrw93/scottrw93.github.io.git
+git clone -i ../deploy/travis git@github.com:scottrw93/scottrw93.github.io.git
 
 rm -rf scottrw93.github.io/*
 cp -r `ls -A | grep -v "scottrw93.github.io"` scottrw93.github.io/
