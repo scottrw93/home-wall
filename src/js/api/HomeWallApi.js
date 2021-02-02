83b186a7afac5724ccdebfbce6d3fbf8334f0754
scottrw93 <scott.rw93@gmail.com>
2021-02-02 16:47:45 +0000

    back to functions

--- src/js/api/HomeWallApi.js
+++ src/js/api/HomeWallApi.js
@@ -1,12 +1,9 @@
-const HOMEWALL_API = 'https://homewall-301021.ew.r.appspot.com';
+const HOMEWALL_API =
+  'https://europe-west3-homewall-301021.cloudfunctions.net/HomeWall';
 //const HOMEWALL_API = 'http://localhost:8080';
 
 export const fetchProblems = () => {
-  return fetch(`${HOMEWALL_API}/problems`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
+  return fetch(`${HOMEWALL_API}/problems`)
     .then((res) => res.json())
     .then(
       (result) => {
@@ -21,9 +18,6 @@ export const fetchProblems = () => {
 export const deleteProblem = (uuid) => {
   return fetch(`${HOMEWALL_API}/problems/${uuid}`, {
     method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
   }).then(
     (result) => {
       return result;
@@ -38,9 +32,6 @@ export const createProblem = (problem) => {
   return fetch(`${HOMEWALL_API}/problems`, {
     method: 'POST',
     body: JSON.stringify(problem),
-    headers: {
-      'Content-Type': 'application/json',
-    },
   })
     .then((res) => res.json())
     .then(
@@ -54,11 +45,7 @@ export const createProblem = (problem) => {
 };
 
 export const fetchWalls = () => {
-  return fetch(`${HOMEWALL_API}/walls`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
+  return fetch(`${HOMEWALL_API}/walls`)
     .then((res) => res.json())
     .then(
       (result) => {
@@ -73,9 +60,6 @@ export const fetchWalls = () => {
 export const createWall = (wall) => {
   return fetch(`${HOMEWALL_API}/walls`, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
     body: JSON.stringify(wall),
   })
     .then((res) => res.json())
@@ -90,12 +74,9 @@ export const createWall = (wall) => {
 };
 
 export const uploadWallImage = (file) => {
-  const form = new FormData();
-  form.append('file', file);
-
-  return fetch(`${HOMEWALL_API}/images/upload`, {
+  return fetch(`${HOMEWALL_API}/images`, {
     method: 'POST',
-    body: form,
+    body: file,
   })
     .then((response) => response.json())
     .then((result) => {
